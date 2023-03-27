16a85fb80122f5add42f753d6c2af7d101eda4c4
enkhe-D <enkhedeny07@gmail.com>
2023-03-27 09:21:49 +0200

    test si le src/static/soft-tech est prise en compte ou pas

--- src/techAss/apps.py
+++ src/techAss/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class TechassConfig(AppConfig):
+class TechAssConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'techAss'
--- src/techAssistant/settings.py
+++ src/techAssistant/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'techAss.apps.TechassConfig',
+    'techAss.apps.TechAssConfig',
 ]
 
 MIDDLEWARE = [
@@ -64,7 +64,6 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'techAss'
             ],
         },
     },
