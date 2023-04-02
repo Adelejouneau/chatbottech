e7bf834b73c89476b45cbe48c94780bfb076676b
Adèle Jouneau <121813874+Adelejouneau@users.noreply.github.com>
2023-04-03 01:45:03 +0200

    modifs week end

--- src/chatbot/viewsold.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from .models import Question
-# Create your views here.
-
-def chatbot_view(request):
-    questions = Question.objects.all()
-    return render(request, "chatbot.html", {"questions": questions})
